2d3d317ae840a99a1b2c8d8027775721acae5aea
Flynnemon <flynnemon@users.noreply.github.com>
2018-04-03 22:37:07 -0700

    Update acdoc.js

--- bin/acdoc.js
+++ bin/acdoc.js
@@ -4,8 +4,8 @@
 * Created by: 			Ryan Flynn
 * Created Date: 		Wed, 04 Apr 2018 03:49:43 GMT
 * Date last edited:		Wed, 04 Apr 2018 05:34:25 GMT
-* Edited last by:		sdfRyan Flynn
-* Contributors: 		Ryan Flynn, Adam Link, sdfRyan Flynn
+* Edited last by:		Ryan Flynn
+* Contributors: 		Ryan Flynn, Adam Link
 */
 
 const fullname = require('fullname');
