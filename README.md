9a29348b074abe54ff88505265237f09e77021e1
sdfRyan Flynn <rflynn@chiki.io>
2018-04-03 22:04:12 -0700

    initial

---
+++ README.md
@@ -0,0 +1,25 @@
+# ACdoc
+
+`ACdoc`, or Accountibility Document Block, is a simple tool designed to be tied to a git pre commit hook to automatically update some information about the files you have added to the git commit. `ACdoc` can also be ran in any git folder or sub-folder and manualy invoke the same results. This is all designed to give teams and organizations better accountibility over their code. By keeping some simple fields up to date in a docblock at the top of all your code files, you will gain an important view over every bit of work done.
+
+the ACdoc docblock looks someting like this:
+```
+/*
+* Created by: 			Ryan Flynn
+* Created Date: 		Wed, 04 Apr 2018 03:49:43 GMT
+* Date last edited:		Wed, 04 Apr 2018 04:25:12 GMT
+* Edited last by:		Ryan Flynn
+* Contributors: 		Ryan Flynn, Adam Link
+*/
+```
+It would be best to tie this into your IDE or code generators to keep formatting consistant, but will have no issues filling in the data so long as the keys exist. `ACdoc` will find these keys and keep contributors, edited last by, and date last edited up to date.
+
+Currently `ACdoc` only supports editing existing AC Blocks but will soon have the ability to write blocks to files as well.
+
+### Install
+Currently there is no NPM repository for this, as I will wait for a bit more substance and error handling before i do. So to install, clone this repo, and navigate into the repo directory. After, run the following:
+```
+npm install -g .
+```
+After that install will be complete, linking an executable path to the local development folder. Just run `acdoc` and youre off!
+
---
+++ bin/acdoc.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+/*
+* Created by: 			Ryan Flynn
+* Created Date: 		Wed, 04 Apr 2018 03:49:43 GMT
+* Date last edited:		Wed, 04 Apr 2018 04:25:12 GMT
+* Edited last by:		Ryan Flynn
+* Contributors: 		Ryan Flynn, Adam Link
+*/
+
+const fullname = require('fullname');
+const fs = require('fs');
+const path = require('path');
+const sgf = require("staged-git-files");
+const _ = require('lodash');
+
+const slashComments = ['.js', '.jsx', '.scss'];
+const hashComments = [];
+const htmlComments = [];
+
+const writeFile = (file, name) => {
+	let matches;
+	fs.readFile(file, 'utf8', (err,doc) => {
+		if (err) {
+			return console.log(err);
+		}
+		try {
+			// change last edited date
+			matches = doc.match(/.*([Dd]ate last edited:\s*(.*))\r?\n/)
+			const dateLine = matches[1]
+			const date = matches[2]
+			doc = doc.replace(dateLine, dateLine.replace(date, "") + new Date().toUTCString())
+
+			// change last user to edit field
+			matches = doc.match(/.*([Ee]dited last by:\s*(.*))\r?\n/)
+			const userLine = matches[1]
+			const user = matches[2]
+			doc = doc.replace(userLine, userLine.replace(user, name))
+
+			// modify contributers list
+			matches = doc.match(/.*([Cc]ontributors:\s*(.*))\r?\n/)
+			const contribLine = matches[1]
+			const contribs = matches[2]
+			if(contribs.indexOf(name) < 0) {
+				doc = doc.replace(contribLine, contribLine.replace(contribs, `${contribs}, ${name}`))
+			}
+
+			fs.writeFile(file, doc, 'utf8', (err) => {
+				if (err) return console.log(err);
+			});
+		} catch(err) {
+			return
+		}
+	});
+};
+
+fullname().then(name => {
+		sgf('A', ((err, data) => {
+		_.forEach(data, (file) => {
+			let abPath = path.resolve(file.filename);
+			if( slashComments.indexOf(path.extname(abPath)) > -1 ){
+				writeFile(abPath, name)
+				console.log(`Updating accountibility docblock for ${file.filename}`);
+			}
+		});
+	}))
+}).catch(err => {
+	console.log(err)
+});
